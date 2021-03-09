36ab7739c548129cee882ece5c376eb516cff72d
agustincaro <agustin.robuco@gmail.com>
2021-03-09 19:29:34 -0300

    Add Initial config

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("test..")
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req,res)=> {
+    res.render('home');
+})
+
+app.listen(process.env.PORT,() => {
+    console.log(`Serving on port ${process.env.PORT}`)
+})
