30ef9a3152ebd20855777a4dddfb010e94e13f9b
agustincaro <agustin.robuco@gmail.com>
2021-03-09 19:49:41 -0300

    Add db models

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const app = express();
 const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config();
+const mongoose = require('mongoose');
+const Campground = require('./models/campground')
+
+//db connection
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser : true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+//db error checking
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
---
+++ models/campground.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    descripcion: String,
+    location: String
+});
+
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
